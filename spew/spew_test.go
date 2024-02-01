8ec9d16ec5df54fa5e62e511caf3bf127002b60c
Tim Hockin <thockin@google.com>
2024-01-31 19:26:14 -0800

    Stop using deprecated ioutil funcs

--- spew/spew_test.go
+++ spew/spew_test.go
@@ -19,7 +19,6 @@ package spew_test
 import (
 	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"reflect"
 	"runtime"
@@ -111,7 +110,7 @@ var spewTests []spewTest
 // redirStdout is a helper function to return the standard output from f as a
 // byte slice.
 func redirStdout(f func()) ([]byte, error) {
-	tempFile, err := ioutil.TempFile("", "ss-test")
+	tempFile, err := os.CreateTemp("", "ss-test")
 	if err != nil {
 		return nil, err
 	}
@@ -124,7 +123,7 @@ func redirStdout(f func()) ([]byte, error) {
 	os.Stdout = origStdout
 	tempFile.Close()
 
-	return ioutil.ReadFile(fileName)
+	return os.ReadFile(fileName)
 }
 
 func initSpewTests() {
